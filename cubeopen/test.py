24f9809e78fdec318bdb467cc9e921da4ed1a8d0
cubeopen7 <marketbingo@163.com>
2017-03-13 09:53:19 +0800

    test

--- cubeopen/test.py
+++ cubeopen/test.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
 
+import tushare
+import tquant
 from cubeopen.data_source.youpin.market import getYoupinTodayInfo_21007
 
 if __name__ == "__main__":
-    getYoupinTodayInfo_21007()
+    # data = tquant.get_dividend("000002")
+    # data2 = tquant.get_allotment("000002")
+    data3 = tushare.profit_data(year=2016, top=10000)
+    a = 1

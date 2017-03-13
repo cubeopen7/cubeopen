be40af1de80f47abe7af1d31b6c54b4aa55757a2
cubeopen7 <marketbingo@163.com>
2017-03-14 00:12:04 +0800

    update

--- cubeopen/init/run.py
+++ cubeopen/init/run.py
@@ -1,8 +1,10 @@
 # -*- coding:utf8 -*-
 
 from cubeopen.init.mongo import *
+from cubeopen.init.crawler.dividend import *
 
 
 if __name__ == "__main__":
     init_mongo()
     init_calendar()
+    # 分红

bbdecd7f02cf33c150a4b5dc1b0f34dc31171b95
cubeopen7 <marketbingo@163.com>
2017-03-15 00:48:16 +0800

    update

--- cubeopen/init/run.py
+++ cubeopen/init/run.py
@@ -3,6 +3,7 @@
 from cubeopen.init.mysql import *
 from cubeopen.init.mongo import *
 from cubeopen.init.crawler import *
+from cubeopen.data_script.base_info import update_base_info
 
 
 if __name__ == "__main__":
@@ -10,7 +11,8 @@ if __name__ == "__main__":
     init_mysql()
     # Mongodb
     init_mongo()
-    init_calendar()
+    init_calendar() # 日历表
+    update_base_info()  # 基础信息表
     # 分红
     get_dividend_file()
     insert_mongo()

4e9c27c9e375ffc2dfc86e811d3c5fc93e555ffb
garnet <garnetreds7@163.com>
2017-03-17 21:02:43 +0800

    update

--- cubeopen/test.py
+++ cubeopen/test.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 
-import tquant
+# import tquant
+import tushare
 from cubeopen.data_source.youpin.market import getYoupinTodayInfo_21007
 
 if __name__ == "__main__":
     # a = getYoupinTodayInfo_21007()
-    data = tquant.get_financial("600000")
+    # data = tquant.get_financial("600000")
+    data = tushare.get_report_data(2001,4)
     a = 1
